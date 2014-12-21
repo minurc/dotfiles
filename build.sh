9c86bb600afd606301244677c4581fd0439d080b
Milan Uremovic <milan.uremovic@gmail.com>
2014-12-21 23:42:57 +0100

    vimrc

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-cd ~/
+pwd=`pwd`
 
-mkdir -p .vim/colors
-cd .vim/colors
+mv vimrc ~/.vimrc
+
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
 wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
-cd ~/
+cd $pwd
+
+
 
 
